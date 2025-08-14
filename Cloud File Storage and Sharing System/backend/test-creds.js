7f11d20831e519bc96fc4b560ad446262c6ed122
sinchana258 <sinchanat826@gmail.com>
2025-08-15 02:12:09 +0530

    Save local changes before syncing with remote

--- Cloud File Storage and Sharing System/backend/test-creds.js
+++
@@ -1,5 +0,0 @@
-import dotenv from "dotenv";
-dotenv.config();
-
-console.log("AWS_ACCESS_KEY_ID:", process.env.AWS_ACCESS_KEY_ID);
-console.log("AWS_SECRET_ACCESS_KEY:", process.env.AWS_SECRET_ACCESS_KEY ? "Loaded" : "Missing");
